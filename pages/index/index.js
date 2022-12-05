0e5b4b1947dea3daf112916faf684f3362ee1480
likefengzi <2093603976@qq.com>
2022-12-05 08:38:59 +0800

    fix bug

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,12 +10,24 @@ Page({
   onShareAppMessage() {
 
   },
-  onShareTimeline() { 
-    
+  onShareTimeline() {
+
   },
   result: null,
   isClear: false,
+  isOp: false,
+  isEqual: false,
   numBtn: function (e) {
+    if (this.isEqual) {
+      this.isOp = false
+      this.isEqual = false
+      this.result = null
+      this.isClear = false
+      this.setData({ num: "0", op: " " })
+    }
+    this.isOp = false
+    this.isEqual = false
+
     var num = e.target.dataset.val
     if (this.data.num == "0" || this.isClear) {
       this.setData({ num: num })
@@ -25,17 +37,20 @@ Page({
     }
   },
   opBtn: function (e) {
+    this.isEqual = false
     var op = this.data.op
     var num = Number(this.data.num)
     this.setData({ op: e.target.dataset.val })
-    if (this.isClear) {
+    if (this.isOp || this.isClear) {
       return
     }
+    this.isOp = true
     this.isClear = true
     if (this.result === null) {
       this.result = num
       return
     }
+    
     if (op === "+") {
       this.result = this.result + num
     } else if (op === "-") {
@@ -46,15 +61,18 @@ Page({
       this.result = this.result / num
     } else if (op === "%") {
       this.result = this.result % num
+    } else {
+      
     }
     this.setData({ num: this.result + "" })
     if (e.target.dataset.val === "=") {
-      this.num = "0"
-      this.op = " "
-      this.result = null
+      this.isEqual = true
+      //this.result = null
     }
   },
   dotBtn: function () {
+    this.isOp = false
+    this.isEqual = false
     if (this.isClear) {
       this.setData({ num: "0." })
       this.isClear = false
@@ -66,10 +84,14 @@ Page({
     this.setData({ num: this.data.num + "." })
   },
   delBtn: function () {
+    this.isOp = false
+    this.isEqual = false
     var num = this.data.num.substr(0, this.data.num.length - 1)
     this.setData({ num: num === "" ? "0" : num })
   },
   resetBtn: function () {
+    this.isOp = false
+    this.isEqual = false
     this.result = null
     this.isClear = false
     this.setData({ num: "0", op: " " })
